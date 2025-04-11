a9d1472520e884dc68400f645a3d96c92c7c8138
Célio Júnior <celiojunior0110@gmail.com>
2025-04-11 09:42:30 -0300

    voltando ultima config

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
     cols: 30,
   };
 
-  const serverURL = 'https://habitoca.vercel.app';
+  const serverURL = 'http://localhost:4000';
   let authToken = localStorage.getItem('authToken');
 
   async function registerUser(name, email, password) {
